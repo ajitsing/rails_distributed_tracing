a1d0e267972599e44330e772bc4e32498883a215
AjitSingh <jeetsingh.ajit@gmail.com>
2019-05-05 16:11:35 +0530

    Using thread local to store request id

--- lib/rails_distributed_tracing/trace_id_store.rb
+++ lib/rails_distributed_tracing/trace_id_store.rb
@@ -1,15 +1,15 @@
 module DistributedTracing
   class TraceIdStore
     def self.trace_id=(id)
-      @trace_id = id
+      Thread.current[DistributedTracing::TRACE_ID] = id
     end
 
     def self.trace_id
-      @trace_id
+      Thread.current[DistributedTracing::TRACE_ID]
     end
 
     def self.clear!
-      @trace_id = nil
+      Thread.current[DistributedTracing::TRACE_ID] = nil
     end
   end
 end
